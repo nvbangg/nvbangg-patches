84eeb0d9f949657a7d468d9babfaca37d03b0d78
ILoveOpenSourceApplications <117499019+ILoveOpenSourceApplications@users.noreply.github.com>
2026-01-10 04:21:49 +0530

    fix(YouTube - Hide layout components): Fix "Hide ticket shelf" not working when "Hide horizontal shelves" is off (#124)

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/LayoutComponentsFilter.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/LayoutComponentsFilter.java
@@ -46,7 +46,7 @@ public final class LayoutComponentsFilter extends Filter {
     private final StringFilterGroup compactChannelBarInnerButton;
     private final ByteArrayFilterGroup joinMembershipButton;
     private final StringFilterGroup horizontalShelves;
-    private final ByteArrayFilterGroup ticketShelf;
+    private final ByteArrayFilterGroup ticketShelfBuffer;
     private final StringFilterGroup chipBar;
     private final StringFilterGroup channelProfile;
     private final StringFilterGroupList channelProfileGroupList;
@@ -204,7 +204,7 @@ public final class LayoutComponentsFilter extends Filter {
 
         // Playable horizontal shelf header.
         playablesBuffer = new ByteArrayFilterGroup(
-                Settings.HIDE_PLAYABLES,
+                null,
                 "FEmini_app_destination"
         );
 
@@ -290,15 +290,15 @@ public final class LayoutComponentsFilter extends Filter {
         );
 
         horizontalShelves = new StringFilterGroup(
-                Settings.HIDE_HORIZONTAL_SHELVES,
+                null, // Setting is checked in isFiltered()
                 "horizontal_video_shelf.e",
                 "horizontal_shelf.e",
                 "horizontal_shelf_inline.e",
                 "horizontal_tile_shelf.e"
         );
 
-        ticketShelf = new ByteArrayFilterGroup(
-                Settings.HIDE_TICKET_SHELF,
+        ticketShelfBuffer = new ByteArrayFilterGroup(
+                null,
                 "ticket_item.e"
         );
 
@@ -365,6 +365,7 @@ public final class LayoutComponentsFilter extends Filter {
             return false;
         }
 
+
         if (exceptions.matches(path)) return false; // Exceptions are not filtered.
 
         if (matchedGroup == compactChannelBarInner) {
@@ -375,9 +376,19 @@ public final class LayoutComponentsFilter extends Filter {
         }
 
         if (matchedGroup == horizontalShelves) {
-            return contentIndex == 0 && (hideShelves()
-                    || ticketShelf.check(buffer).isFiltered()
-                    || playablesBuffer.check(buffer).isFiltered());
+            if (contentIndex != 0) return false;
+            final boolean hideShelves = Settings.HIDE_HORIZONTAL_SHELVES.get();
+            final boolean hideTickets = Settings.HIDE_TICKET_SHELF.get();
+            final boolean hidePlayables = Settings.HIDE_PLAYABLES.get();
+
+            if (!hideShelves && !hideTickets && !hidePlayables) return false;
+
+            // Must always check other buffers first, to prevent incorrectly hiding them
+            // if they are set to show but hide horizontal shelves is set to hidden.
+            if (ticketShelfBuffer.check(buffer).isFiltered()) return hideTickets;
+            if (playablesBuffer.check(buffer).isFiltered()) return hidePlayables;
+
+            return hideShelves && hideShelves();
         }
 
         if (matchedGroup == chipBar) {
